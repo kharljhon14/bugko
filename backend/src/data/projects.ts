a5a3a65be818e8ef78976875579db2611a018d49
karl <enriquezkharl14@gmail.com>
2025-05-15 00:21:58 +0800

    [Feat] Db project functions

--- backend/src/data/projects.ts
+++ backend/src/data/projects.ts
@@ -17,3 +17,40 @@ async function createProject(
     throw new Error(`Something went wrong error code: ${error.code} ${error.message}`);
   }
 }
+
+// Get Project By ID
+async function getProjectById(client: PoolClient, id: number): Promise<DBProject> {
+  try {
+    const results = await client.query<DBProject>('SELECT * FROM projects WHERE id = $1', [id]);
+
+    return results.rows[0];
+  } catch (error: any) {
+    throw new Error(`Something went wrong error code: ${error.code} ${error.message}`);
+  }
+}
+
+// Get Projects By Owner
+async function getProjectsByOwner(client: PoolClient, ownerId: number): Promise<DBProject[]> {
+  try {
+    const results = await client.query<DBProject>('SELECT * FROM projects WHERE owner = $1', [
+      ownerId
+    ]);
+
+    return results.rows;
+  } catch (error: any) {
+    throw new Error(`Something went wrong error code: ${error.code} ${error.message}`);
+  }
+}
+
+// Delete Project
+async function deleteProject(client: PoolClient, id: number) {
+  try {
+    const results = await client.query<DBProject>('DELETE FROM projects WHERE id = $1', [id]);
+
+    if (results.rowCount === 0) {
+      throw new Error(`No project found with id: ${id}`);
+    }
+  } catch (error: any) {
+    throw new Error(`Something went wrong error code: ${error.code} ${error.message}`);
+  }
+}
