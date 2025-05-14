e9797f0a693c54bcc7377f40457ad983e8a21a61
karl <enriquezkharl14@gmail.com>
2025-05-15 00:38:43 +0800

    [Refactor] Added a new controller directory

---
+++ backend/src/controllers/auth.controller.ts
@@ -0,0 +1,66 @@
+import fastify, { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+import { GoogleUser } from '../types/auth';
+import { getUser, createUser, createSSO } from '../data/auth.data';
+import { CreateSSOSchemaType } from '../schemas/auth.schema';
+
+declare module 'fastify' {
+  interface FastifyInstance {
+    googleOAuth2: import('@fastify/oauth2').OAuth2Namespace;
+  }
+}
+
+export function googleAuthHandler(fastify: FastifyInstance) {
+  return async function (request: FastifyRequest, reply: FastifyReply) {
+    const token = await fastify.googleOAuth2.getAccessTokenFromAuthorizationCodeFlow(request);
+
+    const userInfo = (await fetch('https://www.googleapis.com/oauth2/v2/userinfo', {
+      headers: {
+        Authorization: `Bearer ${token.token.access_token}`
+      }
+    }).then((res) => res.json())) as GoogleUser;
+
+    await request.login({ provider: 'google', ...userInfo });
+
+    reply.redirect('/me');
+  };
+}
+export function getUserInformationHandler(fastify: FastifyInstance) {
+  return async function getUserInformationHandler(request: FastifyRequest, reply: FastifyReply) {
+    const user = request.user as GoogleUser;
+
+    const client = await fastify.pg.connect();
+    try {
+      const foundUser = await getUser(client, user.email);
+
+      if (!foundUser) {
+        const newUser = await createUser(client, user);
+
+        const ssoValues: CreateSSOSchemaType = {
+          user_id: newUser.id,
+          provider: 'google',
+          provider_id: user.id
+        };
+
+        await createSSO(client, ssoValues);
+
+        return reply.send({ data: newUser });
+      }
+
+      return reply.send({ data: foundUser });
+    } catch (error) {
+      return reply.code(500).send({ error: error });
+    } finally {
+      client.release();
+    }
+  };
+}
+
+export async function logoutUserHandler(request: FastifyRequest, reply: FastifyReply) {
+  try {
+    await request.logout();
+
+    reply.send({ message: 'logged out' });
+  } catch (error) {
+    return reply.code(500).send({ error: error });
+  }
+}
--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -7,60 +7,16 @@ import { GoogleUser } from '../types/auth';
 import { isAuthenticated } from '../middlewares/auth';
 import { createSSO, createUser, getUser } from '../data/auth.data';
 import { CreateSSOSchemaType } from '../schemas/auth.schema';
-
-declare module 'fastify' {
-  interface FastifyInstance {
-    googleOAuth2: import('@fastify/oauth2').OAuth2Namespace;
-  }
-}
+import {
+  getUserInformationHandler,
+  googleAuthHandler,
+  logoutUserHandler
+} from '../controllers/auth.controller';
 
 export default async function authRoutes(fastify: FastifyInstance) {
-  fastify.get('/login/google/callback', async function (request, reply) {
-    const token = await fastify.googleOAuth2.getAccessTokenFromAuthorizationCodeFlow(request);
+  fastify.get('/login/google/callback', googleAuthHandler(fastify));
 
-    const userInfo = (await fetch('https://www.googleapis.com/oauth2/v2/userinfo', {
-      headers: {
-        Authorization: `Bearer ${token.token.access_token}`
-      }
-    }).then((res) => res.json())) as GoogleUser;
+  fastify.get('/me', { preHandler: [isAuthenticated] }, getUserInformationHandler(fastify));
 
-    await request.login({ provider: 'google', ...userInfo });
-
-    reply.redirect('/me');
-  });
-
-  fastify.get('/me', { preHandler: [isAuthenticated] }, async (request, reply) => {
-    const user = request.user as GoogleUser;
-    console.log(user);
-    const client = await fastify.pg.connect();
-    try {
-      const foundUser = await getUser(client, user.email);
-
-      if (!foundUser) {
-        const newUser = await createUser(client, user);
-
-        const ssoValues: CreateSSOSchemaType = {
-          user_id: newUser.id,
-          provider: 'google',
-          provider_id: user.id
-        };
-
-        await createSSO(client, ssoValues);
-
-        return reply.send({ data: newUser });
-      }
-
-      return reply.send({ data: foundUser });
-    } catch (err) {
-      return reply.code(500).send({ error: err });
-    } finally {
-      client.release();
-    }
-  });
-
-  fastify.get('/logout', { preHandler: [isAuthenticated] }, async (request, reply) => {
-    await request.logout();
-
-    reply.send({ message: 'logged out' });
-  });
+  fastify.get('/logout', { preHandler: [isAuthenticated] }, logoutUserHandler);
 }
